6d963ce4e792cdb4e37857d7e0f27a8fc169a99d
Tomek Juźwiuk <jooseq@gmail.com>
2016-10-19 20:49:39 +0200

    basic routes

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+// CSS 
+import './css/style.css';
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+import Teachers from './components/Teachers';
+import Courses from './components/Courses';
+
+// Render
+render((
+  <Router history={hashHistory}>
+    <Route component = {App} >
+	    <Route path="/" component = {Home} />
+	    <Route path="/about" component = {About} />
+	    <Route path="/teachers" component = {Teachers} />
+	    <Route path="/courses" component = {Courses} />
+	  </Route>
+
+  </Router>
+  ), document.getElementById('root')
+);
